4e28fc18ed9c99cc35e2fffe8b7aead909ede534
ivan770 <leshenko.ivan770@gmail.com>
2020-05-10 21:46:14 +0300

    Add payload::* and MessageBuilder documentation

--- spartan_lib/src/core/payload/dispatchable.rs
+++ spartan_lib/src/core/payload/dispatchable.rs
@@ -1,6 +1,45 @@
 use super::Identifiable;
 
+/// Interface for working with dispatchable messages
 pub trait Dispatchable: Identifiable {
+    /// Check if current message is obtainable
+    ///
+    /// ```
+    /// use spartan_lib::core::message::builder::MessageBuilder;
+    /// use spartan_lib::core::payload::Dispatchable;
+    /// use spartan_lib::chrono::{Utc, Duration};
+    ///
+    /// let message = MessageBuilder::default().body(b"Hello, world").compose().unwrap();
+    /// let delayed_message = MessageBuilder::default()
+    ///     .body(b"Hello, world")
+    ///     .delay(|tz| (Utc::now() + Duration::minutes(10)).timestamp())
+    ///     .compose()
+    ///     .unwrap();
+    ///
+    /// assert!(message.obtainable());
+    /// assert!(!delayed_message.obtainable());
+    /// ```
     fn obtainable(&self) -> bool;
+
+    /// Check if current message is garbage
+    ///
+    /// ```
+    /// use spartan_lib::core::message::builder::MessageBuilder;
+    /// use spartan_lib::core::payload::{Dispatchable, Status};
+    /// use std::thread::sleep;
+    /// use std::time::Duration;
+    ///
+    /// let mut message = MessageBuilder::default()
+    ///     .body(b"Hello, world")
+    ///     .timeout(0)
+    ///     .compose()
+    ///     .unwrap();
+    ///
+    /// message.reserve();
+    ///
+    /// sleep(Duration::from_secs(2));
+    ///
+    /// assert!(message.gc());
+    /// ```
     fn gc(&self) -> bool;
 }
--- spartan_lib/src/core/payload/sortable.rs
+++ spartan_lib/src/core/payload/sortable.rs
@@ -1,5 +1,16 @@
+/// Interface for working with sortable messages
 pub trait Sortable {
     type Sort: Ord;
 
+    /// Get message sort key. It's used for message prioritization in queue.
+    ///
+    /// ```
+    /// use spartan_lib::core::message::builder::MessageBuilder;
+    /// use spartan_lib::core::payload::Sortable;
+    ///
+    /// let message = MessageBuilder::default().body(b"Hello, world").compose().unwrap();
+    ///
+    /// dbg!(message.sort());
+    /// ```
     fn sort(&self) -> Self::Sort;
 }
