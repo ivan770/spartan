fade05f7896c2ecd8413bf20273c4236a7faeb78
ivan770 <leshenko.ivan770@gmail.com>
2021-01-03 21:38:52 +0200

    Access middleware for warp

--- spartan/http/middleware/access.rs
+++ spartan/http/middleware/access.rs
@@ -1,20 +1,16 @@
-use std::{
-    future::{ready, Future, Ready},
-    pin::Pin,
-    task::{Context, Poll},
-};
+use std::sync::Arc;
 
-use actix_service::{Service, Transform};
-use actix_web::{
-    dev::{ServiceRequest, ServiceResponse},
-    http::StatusCode,
-    Error, ResponseError,
-};
 use thiserror::Error as ThisError;
+use warp::{
+    header::optional,
+    hyper::StatusCode,
+    reject::{custom, Reject},
+    Filter, Rejection,
+};
 
-use crate::config::Config;
+use crate::{actions::RespondableError, config::Config, node::Manager};
 
-#[derive(ThisError, Debug)]
+#[derive(ThisError, Copy, Clone, Debug)]
 pub enum AccessError {
     #[error("Access denied")]
     AccessDenied,
@@ -24,7 +20,7 @@ pub enum AccessError {
     IncorrectKeyHeader,
 }
 
-impl ResponseError for AccessError {
+impl RespondableError for AccessError {
     fn status_code(&self) -> StatusCode {
         match self {
             AccessError::AccessDenied => StatusCode::UNAUTHORIZED,
@@ -35,85 +31,42 @@ impl ResponseError for AccessError {
     }
 }
 
-pub struct Access {
+impl Reject for AccessError {}
+
+pub struct AccessMiddleware {
     config: &'static Config<'static>,
 }
 
-impl Access {
-    pub fn new(config: &'static Config) -> Access {
-        Access { config }
-    }
-}
-
-impl<S, B> Transform<S> for Access
+pub fn access<T>(
+    filter: T,
+) -> impl Filter<Extract = T::Extract, Error = Rejection> + Clone + 'static
 where
-    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
-    S::Future: 'static,
-    B: 'static,
+    T: Filter<Extract = (Arc<Manager<'static>>, String), Error = Rejection> + Clone + 'static,
 {
-    type Request = ServiceRequest;
-    type Response = ServiceResponse<B>;
-    type Error = Error;
-    type InitError = ();
-    type Transform = AccessMiddleware<S>;
-    type Future = Ready<Result<Self::Transform, Self::InitError>>;
-
-    fn new_transform(&self, service: S) -> Self::Future {
-        ready(Ok(AccessMiddleware {
-            service,
-            config: self.config,
-        }))
-    }
+    filter
+        .and(optional("Authorization"))
+        .and_then(
+            move |manager: Arc<Manager<'static>>, queue: String, key: Option<String>| async move {
+                match AccessMiddleware::new(manager.config()).parse_request(key, &queue) {
+                    Ok(_) => Ok((manager, queue)),
+                    Err(e) => Err(custom(e)),
+                }
+            },
+        )
+        .untuple_one()
 }
 
-pub struct AccessMiddleware<S> {
-    service: S,
-    config: &'static Config<'static>,
-}
-
-#[allow(clippy::type_complexity)]
-impl<S, B> Service for AccessMiddleware<S>
-where
-    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
-    S::Future: 'static,
-    B: 'static,
-{
-    type Request = ServiceRequest;
-    type Response = ServiceResponse<B>;
-    type Error = Error;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
-
-    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.service.poll_ready(cx)
+impl AccessMiddleware {
+    fn new(config: &'static Config<'static>) -> Self {
+        AccessMiddleware { config }
     }
 
-    fn call(&mut self, req: ServiceRequest) -> Self::Future {
-        if let Err(error) = self.parse_request(&req) {
-            Box::pin(async move { Err(error.into()) })
-        } else {
-            let fut = self.service.call(req);
-
-            Box::pin(async move { Ok(fut.await?) })
-        }
-    }
-}
-
-impl<S> AccessMiddleware<S>
-where
-    S: Service<Request = ServiceRequest>,
-{
-    fn parse_request(&self, req: &<S as Service>::Request) -> Result<(), AccessError> {
+    fn parse_request(&self, key: Option<String>, queue: &str) -> Result<(), AccessError> {
         if self.has_access_keys() {
-            req.match_info().get("queue").map_or(Ok(()), |queue| {
-                req.headers()
-                    .get("Authorization")
-                    .ok_or(AccessError::AuthorizationHeaderNotFound)?
-                    .to_str()
-                    .map_err(|_| AccessError::IncorrectKeyHeader)?
-                    .strip_prefix("Bearer ")
-                    .map(|token| self.check_access(token, queue))
-                    .ok_or(AccessError::IncorrectKeyHeader)?
-            })
+            key.ok_or(AccessError::AuthorizationHeaderNotFound)?
+                .strip_prefix("Bearer ")
+                .map(|token| self.check_access(token, queue))
+                .ok_or(AccessError::IncorrectKeyHeader)?
         } else {
             Ok(())
         }
@@ -142,21 +95,10 @@ where
 
 #[cfg(test)]
 mod tests {
-    use actix_service::Service;
-    use actix_web::{
-        http::StatusCode,
-        test::{init_service, TestRequest},
-        web::Data,
-        App,
-    };
     use once_cell::sync::Lazy;
+    use warp::{hyper::StatusCode, test::request};
 
-    use crate::{
-        config::{key::Key, Config},
-        http::routing::attach_routes,
-        node::Manager,
-        test_request,
-    };
+    use crate::config::{key::Key, Config};
 
     static CONFIG: Lazy<Config> = Lazy::new(|| Config {
         access_keys: Some(
@@ -185,99 +127,70 @@ mod tests {
 
     macro_rules! init_application {
         ($config:expr) => {
-            App::new()
-                .app_data(Data::new(Manager::new($config)))
-                .configure(|service_config| {
-                    attach_routes($config, service_config);
-                })
+            crate::http::routing::attach_routes(::std::sync::Arc::new(
+                crate::node::manager::Manager::new($config),
+            ))
         };
     }
 
-    #[actix_rt::test]
+    #[tokio::test]
     async fn test_missing_header() {
-        let mut app = init_service(init_application!(&CONFIG)).await;
+        let app = init_application!(&CONFIG);
 
-        let status = app
-            .call(test_request!(get, "/test"))
-            .await
-            .unwrap_err()
-            .as_response_error()
-            .status_code();
+        let resp = request().path("/test").reply(&app).await;
 
-        assert_eq!(status, StatusCode::BAD_REQUEST);
+        assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
     }
 
-    #[actix_rt::test]
+    #[tokio::test]
     async fn test_incorrect_key() {
-        let mut app = init_service(init_application!(&CONFIG)).await;
+        let app = init_application!(&CONFIG);
 
-        let status = app
-            .call(
-                TestRequest::get()
-                    .uri("/test")
-                    .header("Authorization", "Bearer test123")
-                    .to_request(),
-            )
-            .await
-            .unwrap_err()
-            .as_response_error()
-            .status_code();
+        let resp = request()
+            .path("/test2")
+            .header("Authorization", "Bearer test123")
+            .reply(&app)
+            .await;
 
-        assert_eq!(status, StatusCode::UNAUTHORIZED);
+        assert_eq!(resp.status(), StatusCode::UNAUTHORIZED);
     }
 
-    #[actix_rt::test]
+    #[tokio::test]
     async fn test_queue_not_allowed() {
-        let mut app = init_service(init_application!(&CONFIG)).await;
+        let app = init_application!(&CONFIG);
 
-        let status = app
-            .call(
-                TestRequest::get()
-                    .uri("/test2")
-                    .header("Authorization", "Bearer testing")
-                    .to_request(),
-            )
-            .await
-            .unwrap_err()
-            .as_response_error()
-            .status_code();
+        let resp = request()
+            .path("/test2")
+            .header("Authorization", "Bearer testing")
+            .reply(&app)
+            .await;
 
-        assert_eq!(status, StatusCode::UNAUTHORIZED);
+        assert_eq!(resp.status(), StatusCode::UNAUTHORIZED);
     }
 
-    #[actix_rt::test]
+    #[tokio::test]
     async fn test_queue_allowed() {
-        let mut app = init_service(init_application!(&CONFIG)).await;
+        let app = init_application!(&CONFIG);
 
-        let status = app
-            .call(
-                TestRequest::get()
-                    .uri("/test/size")
-                    .header("Authorization", "Bearer testing")
-                    .to_request(),
-            )
-            .await
-            .unwrap()
-            .status();
+        let resp = request()
+            .path("/test/size")
+            .header("Authorization", "Bearer testing")
+            .reply(&app)
+            .await;
 
-        assert_eq!(status, StatusCode::OK);
+        assert_eq!(resp.status(), StatusCode::OK);
     }
 
-    #[actix_rt::test]
+    #[tokio::test]
     async fn test_wildcard() {
-        let mut app = init_service(init_application!(&CONFIG)).await;
+        let app = init_application!(&CONFIG);
 
-        let status = app
-            .call(
-                TestRequest::get()
-                    .uri("/test/size")
-                    .header("Authorization", "Bearer wildcard")
-                    .to_request(),
-            )
-            .await
-            .unwrap()
-            .status();
+        let resp = request()
+            .path("/test/size")
+            .header("Authorization", "Bearer wildcard")
+            .reply(&app)
+            .await;
 
-        assert_eq!(status, StatusCode::OK);
+        assert_eq!(resp.status(), StatusCode::OK);
     }
 }
--- spartan/http/routing.rs
+++ spartan/http/routing.rs
@@ -1,9 +1,10 @@
 // use crate::{config::Config, http::middleware::access::Access};
 use std::{convert::Infallible, sync::Arc};
 
-use warp::{any, body::json, delete, get, path, post, Filter, Rejection, Reply};
+use warp::{any, body::json, delete, get, path, post, wrap_fn, Filter, Rejection, Reply};
 
-use crate::node::Manager;
+use super::middleware::access::{access, AccessError};
+use crate::{actions::ResponseError, node::Manager};
 
 macro_rules! route {
     ($name:ident) => {
@@ -24,37 +25,48 @@ pub fn attach_routes(
     let pop = with_manager(manager.clone())
         .and(get())
         .and(path!(String))
+        .with(wrap_fn(access))
         .map_async(route!(pop));
 
     let push = with_manager(manager.clone())
         .and(post())
         .and(path!(String))
+        .with(wrap_fn(access))
         .and(json())
         .map_async(route!(push));
 
     let delete = with_manager(manager.clone())
         .and(delete())
         .and(path!(String))
+        .with(wrap_fn(access))
         .and(json())
         .map_async(route!(delete));
 
     let requeue = with_manager(manager.clone())
         .and(post())
         .and(path!(String / "requeue"))
+        .with(wrap_fn(access))
         .and(json())
         .map_async(route!(requeue));
 
     let clear = with_manager(manager.clone())
         .and(post())
         .and(path!(String / "clear"))
+        .with(wrap_fn(access))
         .map_async(route!(clear));
 
     let size = with_manager(manager)
         .and(get())
         .and(path!(String / "size"))
+        .with(wrap_fn(access))
         .map_async(route!(size));
 
-    size.or(clear).or(requeue).or(pop).or(push).or(delete)
+    size.or(clear)
+        .or(requeue)
+        .or(pop)
+        .or(push)
+        .or(delete)
+        .recover(handle_rejections)
 }
 
 #[cfg(test)]
@@ -63,4 +75,12 @@ pub fn attach_test_routes(
     manager: Arc<Manager<'static>>,
 ) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
     attach_routes(manager)
+}
+
+pub async fn handle_rejections(rejection: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(error) = rejection.find::<AccessError>() {
+        Ok(ResponseError::from(*error).into_response())
+    } else {
+        Err(rejection)
+    }
 }
